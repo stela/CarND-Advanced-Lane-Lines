6d449171d32ce4d78bf2c9ff66319a2fb867e913
Stefan Larsson <stefan@larsson.nu>
2017-09-09 00:27:59 +0200

    Yes, calibration and undistortion works :)

--- lanelines.py
+++ lanelines.py
@@ -84,8 +84,15 @@ def warper(img, src, dst):
     img_size = (img.shape[1], img.shape[0])
     M = cv2.getPerspectiveTransform(src, dst)
     warped = cv2.warpPerspective(img, M, img_size, flags=cv2.INTER_NEAREST)  # keep same size as input image
-
     return warped
+
+
+def show_distorted_and_undistorted(an_image, mtx, dist):
+    undistorted1 = undistort_image(an_image, mtx, dist)
+    cv2.imshow("undistorted", undistorted1)
+    cv2.imshow("distorted", an_image)
+    cv2.waitKey(10000)
+    cv2.destroyAllWindows()
 
 def main():
     objpoints = []  # 3D points in real world space
@@ -103,7 +110,9 @@ def main():
     mtx, dist = image_calibration_params(an_image, objpoints, imgpoints)
     print("Calibration parameters: \nmtx={}, \ndist={}".format(mtx, dist))
 
+    #show_distorted_and_undistorted(an_image, mtx, dist)
+    #show_distorted_and_undistorted(cv2.imread("test_images/test6.jpg"), mtx, dist)
 
-cv2.destroyAllWindows()
+
 
 main()
