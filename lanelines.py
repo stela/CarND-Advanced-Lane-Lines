5e0cd6c57de3c89343df889a217c23019c9d5b68
Stefan Larsson <stefan@larsson.nu>
2017-09-10 22:49:45 +0200

    Thresholding and start of perspective transformation added

--- lanelines.py
+++ lanelines.py
@@ -1,12 +1,7 @@
 import numpy as np
 import cv2
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
 import glob
-
-
-import tkinter
-
+# importing matplotlib.pyplot causes TK to crash on init :(
 
 
 # prepare object points
@@ -68,23 +63,27 @@ def accumulate_calibration(img, imgpoints, objpoints):
     return
 
 
-# TODO this should run after all {obj,img}points have been found, on (road)images to correct
+# should run after all {obj,img}points have been found, on (road)images to correct
 def image_calibration_params(img, objpoints, imgpoints):
     gray_shape = img.shape[0:2]
     ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray_shape, None, None)
     return mtx, dist
 
+def calibration_params(imgpoints, objpoints):
+    for fname in fnames:
+        # Display the image, with corner-dots but without calibration
+        # drawCorners(fname)
+
+        # Accumulate calibration data
+        img = cv2.imread(fname)
+        accumulate_calibration(img, imgpoints, objpoints)
+    an_image = cv2.imread(fnames[0])
+    mtx, dist = image_calibration_params(an_image, objpoints, imgpoints)
+    return dist, mtx
+
 
 def undistort_image(img, mtx, dist):
     return cv2.undistort(img, mtx, dist, None, mtx)
-
-
-def warper(img, src, dst):
-    # Compute and apply perpective transform
-    img_size = (img.shape[1], img.shape[0])
-    M = cv2.getPerspectiveTransform(src, dst)
-    warped = cv2.warpPerspective(img, M, img_size, flags=cv2.INTER_NEAREST)  # keep same size as input image
-    return warped
 
 
 def show_distorted_and_undistorted(an_image, mtx, dist):
@@ -94,25 +93,73 @@ def show_distorted_and_undistorted(an_image, mtx, dist):
     cv2.waitKey(10000)
     cv2.destroyAllWindows()
 
-def main():
+
+# Originally copied from 30. Color and Gradient
+# assume img in BGR format
+def threshold_pipeline(img, s_thresh=(170, 255), sx_thresh=(20, 100)):
+    img = np.copy(img)
+    # Convert to HSV color space and separate the V channel
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HLS).astype(np.float)
+    l_channel = hsv[:,:,1]
+    s_channel = hsv[:,:,2]
+    # Sobel x
+    sobelx = cv2.Sobel(l_channel, cv2.CV_64F, 1, 0) # Take the derivative in x
+    abs_sobelx = np.absolute(sobelx) # Absolute x derivative to accentuate lines away from horizontal
+    scaled_sobel = np.uint8(255*abs_sobelx/np.max(abs_sobelx))
+
+    # Threshold x gradient
+    sxbinary = np.zeros_like(scaled_sobel)
+    sxbinary[(scaled_sobel >= sx_thresh[0]) & (scaled_sobel <= sx_thresh[1])] = 1
+
+    # Threshold color channel
+    s_binary = np.zeros_like(s_channel)
+    s_binary[(s_channel >= s_thresh[0]) & (s_channel <= s_thresh[1])] = 1
+    # Stack each channel
+    # Note color_binary[:, :, 0] is all 0s, effectively an all black image. It might
+    # be beneficial to replace this channel with something else.
+    color_binary = np.dstack(( np.zeros_like(sxbinary), sxbinary, s_binary))
+
+    combined_binary = np.zeros_like(sxbinary)
+    combined_binary[(s_binary == 1) | (sxbinary == 1)] = 1
+
+    return color_binary, combined_binary
+
+
+# Originally copied from "15. Transform a Stop Sign"
+def dashboard_to_overhead(img, src, dst):
+    # Compute and apply perspective transform
+    img_size = (img.shape[1], img.shape[0])  # keep same size as input image
+    M = cv2.getPerspectiveTransform(src, dst)
+    warped = cv2.warpPerspective(img, M, img_size, flags=cv2.INTER_NEAREST)
+    return warped
+
+
+
+def lanelines_main():
     objpoints = []  # 3D points in real world space
     imgpoints = []  # 2D points in image plane
 
-    for fname in fnames:
-        # Display the image, with corner-dots but without calibration
-        # drawCorners(fname)
-
-        # Accumulate calibration data
-        img = cv2.imread(fname)
-        accumulate_calibration(img, imgpoints, objpoints)
-
-    an_image = cv2.imread(fnames[0])
-    mtx, dist = image_calibration_params(an_image, objpoints, imgpoints)
-    print("Calibration parameters: \nmtx={}, \ndist={}".format(mtx, dist))
-
+    dist, mtx = calibration_params(imgpoints, objpoints)
+    #print("Calibration parameters: \nmtx={}, \ndist={}".format(mtx, dist))
     #show_distorted_and_undistorted(an_image, mtx, dist)
     #show_distorted_and_undistorted(cv2.imread("test_images/test6.jpg"), mtx, dist)
 
 
+    # TODO threshold image by combining sobel ops + color space conversion
+    img = cv2.imread("test_images/test5.jpg")
+    #img = cv2.imread("test_images/straight_lines1.jpg")
+    color_binary, combined_binary = threshold_pipeline(img)
 
-main()
+    # cv2.imshow("bluegreen-thresholds", color_binary)
+    #cv2.imshow("combined-thresholds", combined_binary)
+    #cv2.waitKey(20000)
+
+    cv2.waitKey(20000)
+
+    # TODO watch walkthrough from around 26:29 and forward:
+    # https://www.youtube.com/watch?v=vWY8YUayf9Q&list=PLAwxTw4SYaPkz3HerxrHlu1Seq8ZA7-5P&index=4
+    cv2.destroyAllWindows()
+
+
+#if __name__ == "__main__":
+#    lanelines_main()
