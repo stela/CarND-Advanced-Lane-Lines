06add2ac960c549a2351e4da9574860ab8e2630b
Stefan Larsson <stefan@larsson.nu>
2017-09-20 01:50:09 +0200

    line/curve param detection

--- lanelines.py
+++ lanelines.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2
 import glob
 # importing matplotlib.pyplot causes TK to crash on init :(
-
+import matplotlib.pyplot as plt
 
 # prepare object points
 nx = 9
@@ -125,7 +125,7 @@ def threshold_pipeline(img, s_thresh=(170, 255), sx_thresh=(20, 100)):
     return color_binary, combined_binary
 
 
-# Originally copied from "15. Transform a Stop Sign"
+# Originally from "15. Transform a Stop Sign"
 def dashboard_to_overhead(img, src, dst):
     # Compute and apply perspective transform
     img_size = (img.shape[1], img.shape[0])  # keep same size as input image
@@ -133,6 +133,81 @@ def dashboard_to_overhead(img, src, dst):
     warped = cv2.warpPerspective(img, M, img_size, flags=cv2.INTER_NEAREST)
     return warped
 
+
+# Originally from "33. Finding the Lines"
+def find_lane_lines(binary_warped):
+    # Assuming you have created a warped binary image called "binary_warped"
+    # Take a histogram of the bottom half of the image
+    histogram = np.sum(binary_warped[binary_warped.shape[0]/2:,:], axis=0)
+    # Create an output image to draw on and  visualize the result
+    out_img = np.dstack((binary_warped, binary_warped, binary_warped))*255
+    # Find the peak of the left and right halves of the histogram
+    # These will be the starting point for the left and right lines
+    midpoint = np.int(histogram.shape[0]/2)
+    leftx_base = np.argmax(histogram[:midpoint])
+    rightx_base = np.argmax(histogram[midpoint:]) + midpoint
+
+    # Choose the number of sliding windows
+    nwindows = 9
+    # Set height of windows
+    window_height = np.int(binary_warped.shape[0]/nwindows)
+    # Identify the x and y positions of all nonzero pixels in the image
+    nonzero = binary_warped.nonzero()
+    nonzeroy = np.array(nonzero[0])
+    nonzerox = np.array(nonzero[1])
+    # Current positions to be updated for each window
+    leftx_current = leftx_base
+    rightx_current = rightx_base
+    # Set the width of the windows +/- margin
+    margin = 100
+    # Set minimum number of pixels found to recenter window
+    minpix = 50
+    # Create empty lists to receive left and right lane pixel indices
+    left_lane_inds = []
+    right_lane_inds = []
+
+    # Step through the windows one by one
+    for window in range(nwindows):
+        # Identify window boundaries in x and y (and right and left)
+        win_y_low = binary_warped.shape[0] - (window+1)*window_height
+        win_y_high = binary_warped.shape[0] - window*window_height
+        win_xleft_low = leftx_current - margin
+        win_xleft_high = leftx_current + margin
+        win_xright_low = rightx_current - margin
+        win_xright_high = rightx_current + margin
+        # Draw the windows on the visualization image
+        cv2.rectangle(out_img,(win_xleft_low,win_y_low),(win_xleft_high,win_y_high),
+                      (0,255,0), 2)
+        cv2.rectangle(out_img,(win_xright_low,win_y_low),(win_xright_high,win_y_high),
+                      (0,255,0), 2)
+        # Identify the nonzero pixels in x and y within the window
+        good_left_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) &
+                          (nonzerox >= win_xleft_low) &  (nonzerox < win_xleft_high)).nonzero()[0]
+        good_right_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) &
+                           (nonzerox >= win_xright_low) &  (nonzerox < win_xright_high)).nonzero()[0]
+        # Append these indices to the lists
+        left_lane_inds.append(good_left_inds)
+        right_lane_inds.append(good_right_inds)
+        # If you found > minpix pixels, recenter next window on their mean position
+        if len(good_left_inds) > minpix:
+            leftx_current = np.int(np.mean(nonzerox[good_left_inds]))
+        if len(good_right_inds) > minpix:
+            rightx_current = np.int(np.mean(nonzerox[good_right_inds]))
+
+    # Concatenate the arrays of indices
+    left_lane_inds = np.concatenate(left_lane_inds)
+    right_lane_inds = np.concatenate(right_lane_inds)
+
+    # Extract left and right line pixel positions
+    leftx = nonzerox[left_lane_inds]
+    lefty = nonzeroy[left_lane_inds]
+    rightx = nonzerox[right_lane_inds]
+    righty = nonzeroy[right_lane_inds]
+
+    # Fit a second order polynomial to each
+    left_fit = np.polyfit(lefty, leftx, 2)
+    right_fit = np.polyfit(righty, rightx, 2)
+    return left_fit, right_fit
 
 
 def lanelines_main():
@@ -161,13 +236,19 @@ def lanelines_main():
     src = np.float32([[610, 441], [669, 441], [258, 682], [1049, 682]])
     dst = np.float32([[450, 0], [width - 450, 0], [450, height], [width-450, height]])
 
+#    binary_warped = dashboard_to_overhead(color_binary, src, dst)
+    binary_warped = dashboard_to_overhead(original_img, src, dst)
 
-
-    overhead_img = dashboard_to_overhead(color_binary, src, dst)
+    # TODO sliding window polyfit here, pre-existing src in course, "33. Finding the Lines"
+    
 
     cv2.imshow("undistorted", undistorted_img)
     cv2.imshow("bluegreen-thresholds", color_binary)
-    cv2.imshow("overhead", overhead_img)
+    cv2.imshow("overhead", binary_warped)
+
+    left_fit, right_fit = find_lane_lines(binary_warped)
+    print(left_fit)
+    print(right_fit)
 
     cv2.waitKey(200000)
 
