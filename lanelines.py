7594e5815cbd974ad25c6c921b2058ed6213cd84
Stefan Larsson <stefan@larsson.nu>
2017-09-21 00:08:31 +0200

    Left and right lane lines are identified. TODO: plot tracking-boxes on image

--- lanelines.py
+++ lanelines.py
@@ -138,14 +138,15 @@ def dashboard_to_overhead(img, src, dst):
 def find_lane_lines(binary_warped):
     # Assuming you have created a warped binary image called "binary_warped"
     # Take a histogram of the bottom half of the image
-    histogram = np.sum(binary_warped[binary_warped.shape[0]/2:,:], axis=0)
-    # Create an output image to draw on and  visualize the result
-    out_img = np.dstack((binary_warped, binary_warped, binary_warped))*255
+    bottom_half_y = np.int(binary_warped.shape[0]/2)
+    histogram = np.sum(binary_warped[bottom_half_y:,:], axis=0)
+
     # Find the peak of the left and right halves of the histogram
     # These will be the starting point for the left and right lines
     midpoint = np.int(histogram.shape[0]/2)
-    leftx_base = np.argmax(histogram[:midpoint])
-    rightx_base = np.argmax(histogram[midpoint:]) + midpoint
+    quarter_point = np.int(midpoint/2)
+    leftx_base = np.argmax(histogram[quarter_point:midpoint]) + quarter_point
+    rightx_base = np.argmax(histogram[midpoint:midpoint+quarter_point]) + midpoint
 
     # Choose the number of sliding windows
     nwindows = 9
@@ -159,9 +160,9 @@ def find_lane_lines(binary_warped):
     leftx_current = leftx_base
     rightx_current = rightx_base
     # Set the width of the windows +/- margin
-    margin = 100
+    margin = 80
     # Set minimum number of pixels found to recenter window
-    minpix = 50
+    minpix = 40
     # Create empty lists to receive left and right lane pixel indices
     left_lane_inds = []
     right_lane_inds = []
@@ -175,11 +176,7 @@ def find_lane_lines(binary_warped):
         win_xleft_high = leftx_current + margin
         win_xright_low = rightx_current - margin
         win_xright_high = rightx_current + margin
-        # Draw the windows on the visualization image
-        cv2.rectangle(out_img,(win_xleft_low,win_y_low),(win_xleft_high,win_y_high),
-                      (0,255,0), 2)
-        cv2.rectangle(out_img,(win_xright_low,win_y_low),(win_xright_high,win_y_high),
-                      (0,255,0), 2)
+
         # Identify the nonzero pixels in x and y within the window
         good_left_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) &
                           (nonzerox >= win_xleft_low) &  (nonzerox < win_xleft_high)).nonzero()[0]
@@ -207,7 +204,20 @@ def find_lane_lines(binary_warped):
     # Fit a second order polynomial to each
     left_fit = np.polyfit(lefty, leftx, 2)
     right_fit = np.polyfit(righty, rightx, 2)
-    return left_fit, right_fit
+    return left_fit, right_fit, left_lane_inds, right_lane_inds, nonzerox, nonzeroy
+
+
+def visualize_lane_lines(binary_warped, left_fit, right_fit, nonzerox, nonzeroy, left_lane_inds, right_lane_inds):
+    # Generate x and y values for plotting
+    ploty = np.linspace(0, binary_warped.shape[0]-1, binary_warped.shape[0])
+    left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
+    right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
+
+    # TODO prettify out_img a bit?
+    out_img = np.dstack((binary_warped, binary_warped, binary_warped))*255
+    out_img[nonzeroy[left_lane_inds], nonzerox[left_lane_inds]] = [255, 0, 0]
+    out_img[nonzeroy[right_lane_inds], nonzerox[right_lane_inds]] = [0, 0, 255]
+    return out_img, ploty, left_fitx, right_fitx
 
 
 def lanelines_main():
@@ -237,18 +247,18 @@ def lanelines_main():
     dst = np.float32([[450, 0], [width - 450, 0], [450, height], [width-450, height]])
 
 #    binary_warped = dashboard_to_overhead(color_binary, src, dst)
-    binary_warped = dashboard_to_overhead(original_img, src, dst)
+    binary_warped = dashboard_to_overhead(combined_binary, src, dst)
 
     # TODO sliding window polyfit here, pre-existing src in course, "33. Finding the Lines"
-    
+    left_fit, right_fit, left_lane_inds, right_lane_inds, nonzerox, nonzeroy = find_lane_lines(binary_warped)
 
-    cv2.imshow("undistorted", undistorted_img)
-    cv2.imshow("bluegreen-thresholds", color_binary)
-    cv2.imshow("overhead", binary_warped)
+    out_img, left_fitx, right_fitx = visualize_lane_lines(binary_warped, left_fit, right_fit, nonzerox, nonzeroy, left_lane_inds, right_lane_inds)
 
-    left_fit, right_fit = find_lane_lines(binary_warped)
-    print(left_fit)
-    print(right_fit)
+    #cv2.imshow("undistorted", undistorted_img)
+    #cv2.imshow("bluegreen-thresholds", color_binary)
+    #cv2.imshow("overhead", binary_warped)
+    cv2.imshow("out_img", out_img)
+
 
     cv2.waitKey(200000)
 
